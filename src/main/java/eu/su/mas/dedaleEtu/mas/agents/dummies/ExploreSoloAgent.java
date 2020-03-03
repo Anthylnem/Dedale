0382dd8ce9e619e677254d8e455da9f30e4f1583
Anthylnem <anthea.richaume@gmail.com>
2020-03-03 12:35:14 +0100

    Projet clean

---
+++ src/main/java/eu/su/mas/dedaleEtu/mas/agents/dummies/ExploreSoloAgent.java
@@ -0,0 +1,90 @@
+package eu.su.mas.dedaleEtu.mas.agents.dummies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.su.mas.dedale.mas.AbstractDedaleAgent;
+import eu.su.mas.dedale.mas.agent.behaviours.startMyBehaviours;
+import eu.su.mas.dedaleEtu.mas.behaviours.SendBehaviour;
+import eu.su.mas.dedaleEtu.mas.behaviours.ExploSoloBehaviour;
+import eu.su.mas.dedaleEtu.mas.behaviours.ReceiveBehaviour;
+import eu.su.mas.dedaleEtu.mas.knowledge.MapRepresentation;
+import jade.core.behaviours.Behaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+/**
+ * <pre>
+ * ExploreSolo agent. 
+ * It explore the map using a DFS algorithm.
+ * It stops when all nodes have been visited.
+ *  </pre>
+ *  
+ *
+ */
+
+public class ExploreSoloAgent extends AbstractDedaleAgent {
+
+	private static final long serialVersionUID = -6431752665590433727L;
+	private MapRepresentation myMap;
+	
+
+	/**
+	 * This method is automatically called when "agent".start() is executed.
+	 * Consider that Agent is launched for the first time. 
+	 * 			1) set the agent attributes 
+	 *	 		2) add the behaviours
+	 *          
+	 */
+	protected void setup(){
+
+		super.setup();
+		
+		try {
+		
+			DFAgentDescription dfd = new DFAgentDescription();
+			dfd.setName(getAID());
+			ServiceDescription sd = new ServiceDescription();
+
+			sd.setType("explo");
+			sd.setName(getLocalName());
+			dfd.addServices(sd);
+			
+			DFService.register(this, dfd);
+			
+			Thread.sleep(2000);
+			
+			DFAgentDescription dfd1 = new DFAgentDescription();
+			ServiceDescription sd1 = new ServiceDescription();
+			sd1.setType("explo");
+			dfd1.addServices(sd1);
+			DFAgentDescription[] agentsList = DFService.search(this, dfd1);
+		
+		
+		List<Behaviour> lb=new ArrayList<Behaviour>();
+		
+		ExploSoloBehaviour explo = new ExploSoloBehaviour(this,this.myMap);
+		lb.add(explo);
+		
+		lb.add(new SendBehaviour(this,explo,agentsList));
+		lb.add(new ReceiveBehaviour(this,explo));
+		
+		
+		addBehaviour(new startMyBehaviours(this,lb));
+		
+		System.out.println("the  agent "+this.getLocalName()+ " is started");
+		
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+	
+	
+	
+}
---
+++ src/main/java/eu/su/mas/dedaleEtu/mas/behaviours/ReceiveBehaviour.java
@@ -0,0 +1,99 @@
+package eu.su.mas.dedaleEtu.mas.behaviours;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import eu.su.mas.dedale.mas.AbstractDedaleAgent;
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.lang.acl.UnreadableException;
+import jade.util.leap.Serializable;
+
+/**
+ * It receives a message tagged with an 
+ * 
+ * @author Cédric Herpson
+ *
+ */
+public class ReceiveBehaviour extends SimpleBehaviour{
+
+	private static final long serialVersionUID = 9088209402507795289L;
+
+	private boolean finished=false;
+
+	private ExploSoloBehaviour explo;
+	/**
+	 * 
+	 * 
+	 * @param myagent
+	 */
+	public ReceiveBehaviour(final Agent myagent, ExploSoloBehaviour explo) {
+		super(myagent);
+		this.explo = explo;
+
+	}
+
+
+	@SuppressWarnings("unchecked")
+	public void action() {
+		
+		MessageTemplate pingTemplate = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+		ACLMessage pingMsg = this.myAgent.receive(pingTemplate);
+		
+		if (pingMsg != null) {
+			AID sender = pingMsg.getSender();
+			String name = sender.getLocalName();
+			System.out.println(sender);
+			System.out.println(this.myAgent.getLocalName()+" a reçu le ping ");
+		
+			ACLMessage ackPing = new ACLMessage(ACLMessage.AGREE);
+			ackPing.setSender(this.myAgent.getAID());
+			ackPing.setContent("ackPing send map");
+			ackPing.addReceiver(new AID(name,AID.ISLOCALNAME));
+			((AbstractDedaleAgent)this.myAgent).sendMessage(ackPing);
+			System.out.println(this.myAgent.getLocalName()+" a envoyé le ackPing.");			
+			
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			
+			MessageTemplate msgTemplate = MessageTemplate.MatchPerformative(ACLMessage.INFORM);			
+			ACLMessage msg = this.myAgent.receive(msgTemplate);
+			
+			if (msg != null) {
+				try {
+					System.out.println("Receive "+msg.getContentObject());
+									
+					ArrayList<Serializable> messageContent = (ArrayList<Serializable>) msg.getContentObject();
+	
+					Set<String> closedNodes = (Set<String>) messageContent.get(0);
+					ArrayList<ArrayList<String>> edges = (ArrayList<ArrayList<String>>) messageContent.get(1);
+					List<String> openNodes = (List<String>) messageContent.get(2);
+					
+					explo.majNodes(closedNodes,openNodes);
+					explo.setEdges(edges);
+					
+					System.out.println("Agent "+this.myAgent.getLocalName()+ " a reçu le message.");
+					System.out.println(this.myAgent.getLocalName()+"<----Result received from "+msg.getSender().getLocalName()+" ,content= "+msg.getContentObject());
+				} catch (UnreadableException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else{
+				block();// the behaviour goes to sleep until the arrival of a new message in the agent's Inbox.
+			}
+		}
+	}
+
+	public boolean done() {
+		return finished;
+	}
+
+}
+
